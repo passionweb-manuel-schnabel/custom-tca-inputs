a3ce7b42da4ca6ba1d2f0750523dc7ece50b0930
Manuel Schnabel <service@passionweb.de>
2024-03-14 13:13:12 +0100

    [TASK] Update version to 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'service@passionweb.de',
     'author_company' => 'PassionWeb Manuel Schnabel',
     'state' => 'stable',
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.4.99',
-            'form' => '12.0.0-12.4.99',
+            'typo3' => '12.0.0-12.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
